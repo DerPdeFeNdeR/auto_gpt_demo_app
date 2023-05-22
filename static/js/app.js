1837ac971cb89283b9e270433b9ad996a4ecb1bf
Jonathan Edwards <jde2023@Yvettes-MacBook-Pro.local>
2023-05-22 13:20:18 -0400

    Fix app

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,13 @@
 var app = angular.module('mathApp', []);
 
-app.controller('mathController', function($scope, $http) {
+app.controller('mathController', function($scope, $http, $httpParamSerializerJQLike) {
     $scope.submitForm = function() {
         var data = {
             num1: $scope.num1,
             num2: $scope.num2
         };
         var url = '/' + $scope.operation;
-        $http.post(url, data).then(function(response) {
+        $http.post(url, $httpParamSerializerJQLike(data), {headers: {'Content-Type': 'application/x-www-form-urlencoded'}}).then(function(response) {
             if ('result' in response.data) {
                 $scope.result = response.data.result;
                 $scope.error = '';
@@ -18,3 +18,4 @@ app.controller('mathController', function($scope, $http) {
         });
     };
 });
+
